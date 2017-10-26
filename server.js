544608b8a8d70d8e1677061f1a32fc782818a773
szymon.kordiaczynski <szymon.kordiaczynski@gmail.com>
2017-10-26 23:04:37 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+var port = 3000;
+app.set('view engine','hbs');
+
+
+// app.use( (request, response, next) =>
+// {
+//
+// //	response.render('maintenance.hbs');
+// 	console.log('Hi');
+// 	next();
+// });
+
+
+///*
+app.get('/info',(req,resp)=>
+{
+	resp.render('info.hbs',
+{
+	"currentDate" : new Date().getDate()
+});
+});
+
+
+
+app.listen(port,()=>
+{
+console.log(`Listening on port: ${port} `);
+});
